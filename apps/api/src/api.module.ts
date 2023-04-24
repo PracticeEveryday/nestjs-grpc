7806049633fa50fbd977d0a8399b984803c8bc9f
PracticeEveryday <developerforus@gmail.com>
2023-04-24 11:31:55 +0900

    feat: [DH] 프리즈마 셋팅

--- apps/api/src/api.module.ts
+++ apps/api/src/api.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ApiController } from './api.controller';
 import { ApiService } from './api.service';
 import { CustomConfigModule } from 'libs/config/config.module';
+import { PrismaModule } from '@app/prisma';
 
 @Module({
-  imports: [CustomConfigModule],
+  imports: [CustomConfigModule, PrismaModule],
   controllers: [ApiController],
   providers: [ApiService],
 })
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { ApiModule } from './api.module';
 import { ConfigService } from '@nestjs/config';
+import { PrismaService } from '@app/prisma';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiModule);
   const configService = app.get(ConfigService);
   const PORT = configService.getOrThrow('PORT');
+  const prismaService = app.get(PrismaService);
+  prismaService.enableShutdownHook(app);
   if (PORT && typeof PORT === 'number') {
     await app.listen(PORT);
 
